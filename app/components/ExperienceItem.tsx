8f01b97a0f7be50cc511817ced0f8d857f66c182
Anne Lee <leehananne@gmail.com>
2025-10-23 17:07:34 +0100

    trying to fix images not showing up

--- app/components/ExperienceItem.tsx
+++ app/components/ExperienceItem.tsx
@@ -12,7 +12,7 @@ const ExperienceItem = ({role, type, company, dates, imageUrl}: ExperienceProps)
 return (
     <div className="self-stretch h-24 p-2.5 inline-flex justify-start items-start gap-2.5">
         <div className="w-8 h-8 bg-white flex justify-center items-center gap-2.5 overflow-hidden">
-            <img className="w-3.5 self-stretch" src={imageUrl} />
+            <img className="w-3.5 self-stretch" src={imageUrl} alt={`${company} logo`} />
         </div>
         <div className="inline-flex flex-col justify-start items-start gap-[5px]">
             <div className="self-stretch justify-start dark:text-white text-dark-grey text-sm font-bold leading-none">{role}</div>
--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Profile = () => {
   return (
     <div className="w-full self-stretch inline-flex flex-col justify-center items-center gap-5 overflow-hidden">
-    <img className="w-36 h-36 rounded-full" src="https://placehold.co/150x150" />
+    <img className="w-36 h-36 rounded-full" src="https://placehold.co/150x150" alt="Profile picture" />
     <div className="self-stretch flex flex-col justify-start items-start gap-[5px] overflow-hidden">
         <div className="self-stretch text-center justify-start text-3xl font-bold leading-10">Anne Lee</div>
         <div className="self-stretch text-center justify-start text-grey text-sm font-light leading-none">Design Engineer </div>
